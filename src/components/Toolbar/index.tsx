3210386e899932123c768ffc0cfcdbdd91e751b7
John Need <john.need@inulabs.tech>
2023-06-29 10:41:16 -0400

    fix: icons in mac issue

--- src/components/Toolbar/index.tsx
+++ src/components/Toolbar/index.tsx
@@ -1,6 +1,7 @@
 import React, { FormEvent, useState } from "react";
 import styles from "./Toolbar.module.css";
-
+import historyIcon from "../../icon-history.svg";
+import closeIcon from "../../icon-close.svg";
 type ToolbarProps = {
     onSubmit: (searchTerm: string) => void;
     openHistory: () => void;
@@ -23,12 +24,20 @@ export const Toolbar = ({ onSubmit, isResultsPanelOpen, toggleSearchPanel, openH
                        value={searchTerm} onChange={evt => setSearchTerm(evt.target.value)} />
                 <button title={"search"} className={styles["toolbar_submit"]} onClick={search}>&#x1F50E;</button>
             </form>
-            <button disabled={!isResultsPanelOpen} title={"Close Panel"}  className={styles["toolbar_toggle-panel"]} onClick={toggleSearchPanel}>
-                <span>❌</span>
-            </button>
-            <button title={"Search History"}  className={styles["toolbar_show-history"]} onClick={openHistory}>
-                <span className={styles["toolbar_open-search-history"]}>&#128214;</span>
-            </button>
+            <button
+                disabled={!isResultsPanelOpen} title={"Close Panel"}
+                className={styles["toolbar_toggle-panel"]}
+                onClick={toggleSearchPanel}
+                style={{backgroundImage: "url('" +closeIcon+"')"}}
+            />
+
+            <button
+                title={"Search History"}
+                className={styles["toolbar_show-history"]}
+                onClick={openHistory}
+                style={{backgroundImage: "url('" +historyIcon+"')"}}
+            />
+
         </div>
     );
 };
