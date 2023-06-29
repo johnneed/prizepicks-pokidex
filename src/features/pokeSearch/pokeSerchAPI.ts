8ec1c7a4994dfcbc52c144198773e5cfbe40c4ea
John Need <john.need@inulabs.tech>
2023-06-29 10:43:24 -0400

    fix: case search bug

--- src/features/pokeSearch/pokeSerchAPI.ts
+++ src/features/pokeSearch/pokeSerchAPI.ts
@@ -2,5 +2,5 @@ import { PokemonClient } from "pokenode-ts";
 
 const api = new PokemonClient();
 
-export const getPokemonByName = (name: string) => api.getPokemonByName(name);
+export const getPokemonByName = (name: string) => api.getPokemonByName(name.toLowerCase());
 
